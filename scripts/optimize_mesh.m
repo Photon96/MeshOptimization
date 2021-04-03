8586c6373665e41b5e587bd2ad448a6b968dbf39
Lukasz <luukaszp96@gmail.com>
2021-04-03 21:42:54 +0200

    Improve structure of script

--- scripts/optimize_mesh.m
+++ scripts/optimize_mesh.m
@@ -9,9 +9,8 @@ max_k = 1;
 resolution = 1/50; 
 quality_treshold = 0.35;
 
-info.quality_treshold = quality_treshold;
-info.nr_iterations = max_k;
 prev_qualities1 = CalcQualityTetraVLrms(tetras, positions);
+
 opts.max_k = max_k;
 opts.quality_treshold = quality_treshold;
 opts.resolution = resolution;
@@ -23,18 +22,15 @@ info.duration = toc;
 
 current_qualities1 = CalcQualityTetraVLrms(tetras, positions);
 
+info.quality_treshold = quality_treshold;
+info.nr_iterations = max_k;
 info.min_prev_quality = min(prev_qualities1);
 info.mean_prev_quality = mean(prev_qualities1);
 info.min_current_quality = min(current_qualities1);
 info.mean_current_quality = mean(current_qualities1);
-figure(1);
-DrawQualityGraph(prev_qualities1, current_qualities1, '$6 \sqrt{2}\frac{V}{L_{rms}^3}$');
 
-txt = sprintf('Optimization info: %s')
+sprintf("Optimization info:")
 info
 
-% for i=1:length(nodes_optimize)
-%     i
-%     adjacent_tetras = GetAdjacentTetras(nodes_optimize(i), tetras);
-%     CalcQualityTetraVLrms(adjacent_tetras, positions)     
-% end
+figure(1);
+DrawQualityGraph(prev_qualities1, current_qualities1, '$6 \sqrt{2}\frac{V}{L_{rms}^3}$');
