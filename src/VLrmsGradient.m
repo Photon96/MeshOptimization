c16c16258db3b1e9e875ebefa0e9b70bf26bd554
Lukasz <luukaszp96@gmail.com>
2021-04-04 12:12:52 +0200

    Refactor

--- src/VLrmsGradient.m
+++ src/VLrmsGradient.m
@@ -4,10 +4,9 @@ function [grad, grad_Lrms] = VLrmsGradient(vertex, tetras, positions, min_tetr)
         tetras_current = tetras(i, :);
         positions_current = positions(tetras_current, :);
         vertex_current = tetras_current == vertex;
-        fixed_vertices = positions_current;
-        vertex_current_pos = fixed_vertices(vertex_current, :);
-        fixed_vertices(vertex_current, :) = [];
-%         vertex_current_pos = positions_current(vertex_current, :);
+        vertex_current_pos = positions_current(vertex_current, :);
+        fixed_vertices = positions_current(~vertex_current, :);
+
         
         t = fixed_vertices(1,:) - vertex_current_pos;
         u = fixed_vertices(2,:) - vertex_current_pos;
@@ -31,7 +30,6 @@ function [grad, grad_Lrms] = VLrmsGradient(vertex, tetras, positions, min_tetr)
         
         grad_Lrms = -1/(6*Lrms) * (t + u + v);
         
-%         V = abs(det([t; u; v])/6);
         grad_V = cross(u - v,t - v)/6;
         
         grad = 6*sqrt(2)*(grad_V./(Lrms^3) - (3*V*grad_Lrms)./(Lrms^4));
